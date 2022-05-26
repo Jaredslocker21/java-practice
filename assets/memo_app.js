4fdb45b22b504f8400df5dbadc712de7d798e435
Jaredslocker21 <jaredslocker21@gmail.com>
2022-05-26 14:43:14 +0000

    get memofx

--- assets/memo_app.js
+++ assets/memo_app.js
@@ -1,6 +1,10 @@
 let memoContainer = document.getElementById("application")
 let addMemoButton = memosContainer.queryselector(".add-memo")
 
+getMemos().forEach((note) =>{
+    const memoElement = createMemoElement
+});
+
 function getMemos() {
     return JSON.parse(localStorage.getItem("memo-memos") || "[]");
 }
@@ -21,6 +25,13 @@ function createMemoElement(id, content){
         updateMemo(id, element.value);
 });
 
+    element.addEventListener("dblclick", () =>{
+        const doDelete = confirm("delete Y or N")
+        if (doDelete) {
+            deleteMemo(id,element)
+        }
+    })
+
     return element;
 
 }
@@ -29,11 +40,16 @@ function createMemoElement(id, content){
 
 function addMemo(){}
 
-function updateMemo(id, newContent){}
+function updateMemo(id, newContent){
+    console.log ("Update Memo")
+    console.log (id, newContent);
+}
 
 function deleteMemo(id, element){}
+    console.log("Delete Memo")
+    console.log(id, newContent)
 
 
 
 
-addMemoButton.addEventListnener("click", () => addMemoButton());
+
