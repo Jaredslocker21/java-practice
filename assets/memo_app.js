e5ba8e2e81547c1f8d4a4cd5a6353f3296968d2d
Jaredslocker21 <jaredslocker21@gmail.com>
2022-05-26 14:28:31 +0000

    add create element

--- assets/memo_app.js
+++ assets/memo_app.js
@@ -2,16 +2,34 @@ let memoContainer = document.getElementById("application")
 let addMemoButton = memosContainer.queryselector(".add-memo")
 
 function getMemos() {
-    return JSON.parse(localStorage.getItem("memos") || "[]");
+    return JSON.parse(localStorage.getItem("memo-memos") || "[]");
 }
 
-function saveMemo(){}
+function saveMemos(memos){
+    localStorage.setItem("memo-memos", JSON.stringify(memos))
 
-function createMemoElement(id, content){}
+}
+
+function createMemoElement(id, content){
+    const element = document.createElement("textarea");
+
+    element.classList.add("memo")
+    element.value = content;
+    element.placeholder = "Keep Organized";
+
+    element.addEventListener("change", () => {
+        updateMemo(id, element.value);
+});
+
+    return element;
+
+}
+
+
 
 function addMemo(){}
 
-function updateMemon(id, newContent){}
+function updateMemo(id, newContent){}
 
 function deleteMemo(id, element){}
 
