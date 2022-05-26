e4c4521efc58219e88c1ec79aef0b97b2204a1dc
Jaredslocker21 <jaredslocker21@gmail.com>
2022-05-26 11:36:57 +0000

    add most functions for memo part of app

--- assets/memo_app.js
+++ assets/memo_app.js
@@ -1,6 +1,20 @@
 let memoContainer = document.getElementById("application")
 let addMemoButton = memosContainer.queryselector(".add-memo")
 
+function getMemo(){}
+
+function saveMemo(){}
+
+function createMemoElement(id, content){}
+
+function addMemo(){}
+
+function updateMemon(id, newContent){}
+
+function deleteMemo(id, element){}
+
+
+
 
 getMemos().forEach((memo) => {
     const memoElement = createMemoElement(memo.id, note.content)
