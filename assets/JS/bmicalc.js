df484b25281a08704229d501cc45138137e326fb
Jaredslocker21 <jaredslocker21@gmail.com>
2022-06-07 08:36:15 +0000

    fix syntax error in bmi calculator

--- assets/JS/bmicalc.js
+++ assets/JS/bmicalc.js
@@ -15,9 +15,9 @@ function calculateBMI() {
       result.innerHTML = "Invalid Input"
   } else {
     // Fixes up too two decimal points
-    const heigtInMtr = heightInCm/100;
-    const bmi = (weight / (heightInMtr * heightInMtr).toFixed(2);
-    // Results of BMI prints the result
+    const heightInMtr = heightInCm/100;
+    const bmi = (weight / (heightInMtr * heightInMtr)).toFixed(2);
+        // Results of BMI prints the result
     if (bmi < 18.6) {
       result.innerHTML = `Add Calories : <span>${bmi}</span>`;
     } else if (bmi >= 18.6 && bmi < 24.9) {
