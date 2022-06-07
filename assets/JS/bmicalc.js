626d114dee7aa1f681649e84a4912194b61d934f
Jaredslocker21 <jaredslocker21@gmail.com>
2022-06-07 15:24:33 +0000

    add href tag to links in body bmi,index

--- assets/JS/bmicalc.js
+++ assets/JS/bmicalc.js
@@ -20,11 +20,11 @@ function calculateBMI() {
     const bmi = (weight / (heightInMtr * heightInMtr)).toFixed(2);
         // Results of BMI prints the result
     if (bmi < 18.6) {
-      result.innerHTML = `Add Calories : <span>${bmi}</span>`;
+      result.innerHTML = `Underweight: <span>${bmi}</span>`;
     } else if (bmi >= 18.6 && bmi < 24.9) {
-      result.innerHTML = `Normal : <span>${bmi}</span>`;
+      result.innerHTML = `Normal: <span>${bmi}</span>`;
     } else {
-      result.innerHTML = `Decreese Calories : <span>${bmi}</span>`;
+      result.innerHTML = `Overweight: <span>${bmi}</span>`;
     }
   }
 }
